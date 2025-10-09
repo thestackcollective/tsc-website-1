56b13eb4f9d488057a502065370b801267768cbe
Cameron Mackintosh <cbmackintosh@outlook.com>
2025-10-08 19:08:28 -0600

    Add basic JSX elements

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -3,6 +3,23 @@ export default function ContactPage() {
     <form>
       <h2>WANT TO JOIN OUR COMMUNITY OF HAVE ADDITIONAL QUESTIONS?</h2>
       <p>Fill out the form below</p>
+
+      <label for="firstName">First Name:</label>
+      <input type="text" name="firstName"></input>
+
+      <label for="lastName">Last Name:</label>
+      <input type="text" name="lastName"></input>
+
+      <label for="email">Email</label>
+      <input type="text" name="email"></input>
+
+      <label for="referralQuestion">How did you hear about us?</label>
+      <input type="text" name="referralQuestion"></input>
+
+      <label for="message">Write your message/inquiry here:</label>
+      <textarea type="text" name="message"></textarea>
+      
+      <button>SUBMIT</button>
     </form>
   )
 }
