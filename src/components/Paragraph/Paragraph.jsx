bd0cb1e1c62321dfa9261c43a998624cf26b06a3
Stevie Ambroise <s.ambroise@protonmail.com>
2025-10-12 18:24:18 -0400

    Make paragraph component re-usable

--- src/components/Paragraph/Paragraph.jsx
+++ src/components/Paragraph/Paragraph.jsx
@@ -1,3 +1,8 @@
-export default function Paragraph({text}) {
-
+export default function Paragraph({heading, text}) {
+  return (
+    <section className="">
+      <h3>{heading}</h3>
+      <p>{text}</p>
+    </section>
+  )
 }
