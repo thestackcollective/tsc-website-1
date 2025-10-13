4b8ba003d3511b46f02274dfda1d71dc997d8823
Stevie Ambroise <s.ambroise@protonmail.com>
2025-10-13 19:50:11 -0400

    Add the dates to the events

--- src/components/EventTeaserSection/EventTeaserSectionContainer.jsx
+++ src/components/EventTeaserSection/EventTeaserSectionContainer.jsx
@@ -6,19 +6,22 @@ export default function EventTeaserSectionContainer() {
   const EVENTS = [
     {
       eventHeader: 'RollerSkating Ruby',
+      date: 'Oct 9, 2025',
       text: 'This land is my land, this land is your land, From California, to the New York Islands',
       imageSource: '/images/folks_whiteboarding.jpg',
       altText: 'a group of people, two standing in front of a computer, two in front of a blackboard'
     },
     {
       eventHeader: 'Parasailing Python',
+      date: 'Dec 19, 2025',
       text: 'From California, to the New York Islands, From the redwood forests to the spring waters',
       imageSource: 'images/pexels-mikhail-nilov-6894013.jpg',
       altText: 'a woman is sitting cross-legged on a big pillow, with a laptop on her lap'
     },
     {
       eventHeader: 'Nodding off Node',
-      text: 'From the redwood forests to the spring waters',
+      date: 'Jan 19, 2026',
+      text: 'From the redwood forests to the spring waters, this land was made for you and me.',
       imageSource: 'images/woman_coding.jpg',
       altText: 'an image of a woman coding, with a man standing in the background'
     }
@@ -28,6 +31,7 @@ export default function EventTeaserSectionContainer() {
     return (
       <EventCard
         eventHeading={listedEvent.eventHeader}
+        date={listedEvent.date}
         text={listedEvent.text}
         altText={listedEvent.altText}
         image={listedEvent.imageSource}
