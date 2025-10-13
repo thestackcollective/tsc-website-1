6e7f7b44262ad879d73efec6d36d434edf03f93e
Stevie Ambroise <s.ambroise@protonmail.com>
2025-10-12 20:54:27 -0400

    Remove unnecessary divs and styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,18 +5,10 @@ export default function Navbar() {
     <nav>
       <NavLinkButton location="./" text="TheStackCollective" />
       <div className='pages-nav'>
-        <div>
           <NavLinkButton location="./about" text="About" />
-        </div>
-        <div>
           <NavLinkButton location="./events" text="Events" />
-        </div>
-        <div>
           <NavLinkButton location="./curriculum" text="Curriculum" />
-        </div>
-        <div>
           <NavLinkButton location="./contact" text="Contact" />
-        </div>
       </div>
     </nav>
   )
