4877b18a3e23c014a6c0e7c394aab62542b77ef8
Stevie Ambroise <s.ambroise@protonmail.com>
2025-10-13 09:39:52 -0400

    Settle on style for the nav tabs

--- src/components/EventTeaserSection/EventTeaserSectionContainer.jsx
+++ src/components/EventTeaserSection/EventTeaserSectionContainer.jsx
@@ -7,20 +7,20 @@ export default function EventTeaserSectionContainer() {
     {
       eventHeader: 'RollerSkating Ruby',
       text: 'This land is my land, this land is your land, From California, to the New York Islands',
-      imageSource: '/images/background_decorating_image.jpeg',
-      altText: 'an image of a man holding up a cellphone'
+      imageSource: '/images/folks_whiteboarding.jpg',
+      altText: 'a group of people, two standing in front of a computer, two in front of a blackboard'
     },
     {
       eventHeader: 'Parasailing Python',
       text: 'From California, to the New York Islands, From the redwood forests to the spring waters',
-      imageSource: 'images/background_image_decor_4.png',
-      altText: 'an image of a man excitedly smiling'
+      imageSource: 'images/pexels-mikhail-nilov-6894013.jpg',
+      altText: 'a woman is sitting cross-legged on a big pillow, with a laptop on her lap'
     },
     {
       eventHeader: 'Nodding off Node',
       text: 'From the redwood forests to the spring waters',
-      imageSource: 'images/background_image_decoration_2.jpeg',
-      altText: 'image of a group of people jumping up and down for joy'
+      imageSource: 'images/woman_coding.jpg',
+      altText: 'an image of a woman coding, with a man standing in the background'
     }
   ]
 
