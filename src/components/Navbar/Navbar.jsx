9668a1c8f10767c5836998887c519bb80ee797be
Anthony Shellman <atshellman@gmail.com>
2025-11-13 14:23:56 -0700

    Update alt text

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <nav>
       <NavLink to="./">
-        <img src={Logo} alt="The Stack Collective logo" height={100} width={100}/>
+        <img src={Logo} alt="Go to home page" height={100} width={100}/>
       </NavLink>
       <div className='pages-nav'>
           <NavLinkButton location="./about" text="ABOUT" />
