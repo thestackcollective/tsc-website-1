dad76dcff1a93f45a1255c37a7c3d7b37b26ee68
Anthony Shellman <atshellman@gmail.com>
2025-12-03 14:35:54 -0700

    Add placeholder text and home button

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,19 @@
+import "./ErrorPage.css"
+import { NavLink } from 'react-router'
+
 export default function ErrorPage() {
   return (
-    <h1>Error Page!</h1>
+    <section className="error-page">
+      <h1>404</h1>
+      <h2>Page Not Found</h2>
+      <p>You have reached a page that does not exist</p>
+      <div className="error-page-button-container">
+        <NavLink to="/">
+          <button className="error-page-button">
+            Return Home
+          </button>
+        </NavLink>
+      </div>
+    </section>
   )
 }
