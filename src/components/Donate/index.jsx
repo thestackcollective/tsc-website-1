e0aeb20a83448d7e226f9317e9cd785ef2d2eaaf
Rory Magee <roryemagee@gmail.com>
2026-03-01 15:24:12 -0700

    I hope I can figure out this merge conflict

--- src/components/Donate/index.jsx
+++ src/components/Donate/index.jsx
@@ -9,6 +9,8 @@ export default function Donate() {
     <dbox-widget campaign="stackcollective-donation-page" type="donation_form" enable-auto-scroll="true"></dbox-widget>
   `;
 
+  // Please let me fix this!
+
   return (
     <>
       <div className="container donate-page">
@@ -19,12 +21,7 @@ export default function Donate() {
           <div className="donor-box">
             <script type="module" src="https://donorbox.org/widgets.js" async></script>
             <div dangerouslySetInnerHTML={{ __html: donorboxCode }}></div>
-<<<<<<< HEAD
-          </div> */}
->>>>>>> d133f0ca58a2fbf8c072c009241562903b3b3f74
-=======
           </div>
->>>>>>> fb4db6ae0d3a79eb83dd7afe024fe4c21bcfc294
         </div>
         <div className="map-wrap">
           <img src={EngineerPhoto} alt="Stock Image" />
