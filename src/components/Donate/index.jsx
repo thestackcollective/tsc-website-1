e22fcd660f2004db7b4407f5464445d1d9cade71
Rory Magee <roryemagee@gmail.com>
2026-03-12 16:37:58 -0600

    Save temporary donorbox button

--- src/components/Donate/index.jsx
+++ src/components/Donate/index.jsx
@@ -2,13 +2,14 @@ import './index.scss'
 import { Link } from 'react-router'
 
 import EngineerPhoto from '../../assets/images/pexelsengineeryellow.jpg';
+import DonorboxScreenshotButton from '../../assets/images/donorboxscreenshotbutton.png';
 import Widget from './Widget'
 
 export default function Donate() {
 
-  const donorboxCode = `
-    <dbox-widget campaign="stackcollective-donation-page" type="donation_form" enable-auto-scroll="true"></dbox-widget>
-  `;
+  // const donorboxCode = `
+  //   <dbox-widget campaign="stackcollective-donation-page" type="donation_form" enable-auto-scroll="true"></dbox-widget>
+  // `;
 
   return (
     <>
@@ -18,8 +19,11 @@ export default function Donate() {
           <h1>Donate</h1>
           <br />
           <div className="donor-box">
-            <script type="module" src="https://donorbox.org/widgets.js" async></script>
-            <div dangerouslySetInnerHTML={{ __html: donorboxCode }}></div>
+            <Link className="donor-box-button" to="https://donorbox.org/stackcollective-donation-page">
+              <img src={DonorboxScreenshotButton} alt="Donorbox Screenshot Button" />
+            </Link>
+            {/* <script type="module" src="https://donorbox.org/widgets.js" async></script>
+            <div dangerouslySetInnerHTML={{ __html: donorboxCode }}></div> */}
           </div>
           {/* <div id="donor-box-id" className="donor-box"> */}
             {/* <Widget /> */}
